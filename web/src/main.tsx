c439c8d597819300616aaf341d27a0f933b36938
wengweijian <sniggiw@qq.com>
2024-10-08 15:38:00 +0800

    feat:commit

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+// import App from "./App.tsx";
 import "./assets/global.scss";
 
 import { RouterProvider, createRouter } from "@tanstack/react-router";
